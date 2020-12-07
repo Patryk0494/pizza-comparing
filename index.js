8436d4c954c34917e208376a3479320c8b2fc26f
Patryk <jpatryk0494@gmail.com>
2020-12-07 17:25:52 +0100

    functionality improved

--- index.js
+++ index.js
@@ -11,45 +11,56 @@ add.addEventListener("click", function() {
     const outputs = document.getElementsByClassName("outputs");
     if (outputs.length>0) {
         delOutp();
-        const newInput = document.createElement("div");
-        newInput.id = `inputs-${index}`;
-        newInput.innerHTML = `
-            <span>${index+1}.<span>
-            <input type="number" id="diameter-${index}" class="diameter" placeholder="diameter">
-            <input type="number" id="price-${index}" class="price" placeholder="price">
-            `;
-        index++;
-        inpBox.appendChild(newInput);
+        creatInput();
     } else {
-        const newInput = document.createElement("div");
-        newInput.id = `inputs-${index}`;
-        newInput.innerHTML = `
-            <span>${index+1}.<span>
-            <input type="number" id="diameter-${index}" class="diameter" placeholder="diameter">
-            <input type="number" id="price-${index}" class="price" placeholder="price">
-            `;
-        index++;
-        inpBox.appendChild(newInput);
+        creatInput();
     }
+
 })
 
+function creatInput(){
+    const newInput = document.createElement("div");
+    newInput.id = `inputs-${index}`;
+    newInput.classList = `inputs`;
+    newInput.innerHTML = `
+        <span>${index+1}.<span>
+        <input type="number" id="diameter-${index}" class="diameter" placeholder="diameter">
+        <input type="number" id="price-${index}" class="price" placeholder="price">
+        `;
+    index++;
+    inpBox.appendChild(newInput);
+}
+
 let pizzaArr = [];
+let pizzaArrSorted;
 
 count.addEventListener("click", function() {
-    let pizzaArrSorted;
+    
+    if (pizzaArr.length > 0) {
+        for (let i = pizzaArr.length; i < index; i++){
+            sorting(i);
+        }
+    } else {
+        for (let i = 0; i < index; i++) {
+            sorting(i);
+    }}
+    
 
-    for (let i = 0; i < index; i++) {
+    function sorting(i){
         const diameterElem = document.getElementById(`diameter-${i}`);
         const priceElem = document.getElementById(`price-${i}`);
 
-        const radius = (diameterElem.value)/2;
+        const diameter = diameterElem.value
+        const radius = (diameter)/2;
         const price = priceElem.value;
         const ordinal = i+1;
         let calculate = price/(Math.PI * (radius ** 2));
 
         const pizza = {
             index: ordinal,
-            uPrice: calculate
+            price: price,
+            uPrice: calculate,
+            diameter: diameter
         }
 
         pizzaArr.push(pizza);
@@ -58,27 +69,33 @@ count.addEventListener("click", function() {
             return a.uPrice -  b.uPrice;
         });
     }
+
+    console.log(pizzaArrSorted);
     
-    pizzaArrSorted.forEach(function(elem){
-        const newContainer = document.createElement("div");
-        newContainer.classList = "outputs"
-        newContainer.innerHTML = `<p>${elem.index}. ${elem.uPrice.toFixed(3)*10} zł/10cm${"2".sup()}</p>` 
-        outpBox.appendChild(newContainer);
-        console.log()
-        for (let i = 1; i < index; i++) {
-            const inputToDelete = document.getElementById(`inputs-${i}`);
-            inputToDelete?.remove();
-        }
+    pizzaArrSorted.forEach(function(elem, index){
+        const sortedInput = document.getElementById(`inputs-${index}`)
+        sortedInput.classList= 'rotate inputs';
+        setTimeout (function(){
+            sortedInput.innerHTML = `
+            <span>${elem.index}.</span>
+            <input type="number" id="diameter-${elem.index}" value="${elem.diameter}" class="diameter" placeholder="diameter">
+            <input type="number" id="price-${elem.index}" value="${elem.price}" class="price" placeholder="price">
+            <p class="unityPrice">${elem.uPrice.toFixed(3)*10} zł/10cm${"2".sup()}</p>
+            `;
+        }, 300
+        );
+        
     });
-    index = 1;
-    pizzaArr = [];
+    // index = 1;
+    // pizzaArr = [];
 })
 
-function delOutp() {
-    outpBox.innerHTML = ``;
-    index = 1;
+function delInp() {
+    inpBox.innerHTML= "";
+    index = 0;
+    creatInput();
     pizzaArr = [];
 }
 
-erase.addEventListener("click", delOutp);
+erase.addEventListener("click", delInp);
 
